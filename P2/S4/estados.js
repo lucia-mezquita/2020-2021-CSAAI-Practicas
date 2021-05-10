f10a8296c3b32c2b7f5a0811c74c5456a88167f3
lucia-mezquita <luciamezquita@hotmail.es>
2021-05-10 11:54:25 +0200

    s4

---
+++ P2/S4/estados.js
@@ -0,0 +1,140 @@
+console.log("Ejecutando JS...");
+
+display = document.getElementById("display")
+suma = document.getElementById("suma")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
+
+//-- Estados de la calculadora
+const ESTADO = {
+    INIT: 0,
+    OP1: 1,
+    OPERATION: 2,
+    OP2: 3
+}
+ 
+ //-- Variable de estado de la calculadora
+ //-- Al comenzar estamos en el estado incial
+ let estado = ESTADO.INIT;   
+
+//-- Función de retrollamada de los digitos
+function digito(ev)
+{
+    //-- Se ha recibido un dígito
+    //-- Según en qué estado se encuentre la calculadora
+    //-- se hará una cosa u otra
+
+    //-- Si es el primer dígito, no lo añadimos,
+    //-- sino que lo mostramos directamente en el display
+    if (estado == ESTADO.INIT) {
+
+        display.innerHTML = ev.target.value;
+
+        //-- Pasar al siguiente estado
+        estado = ESTADO.OP1;
+
+    } else {
+       
+        //--En cualquier otro estado lo añadimos
+        display.innerHTML += ev.target.value;
+
+        //-- Y nos quedamos en el mismo estado
+        //-- Ojo! Este ejemplo sólo implementa el primer
+        //-- estado del diagrama. Habría que tener en 
+        //-- cuenta el resto... lo debes hacer en tu práctica
+    } 
+    
+}
+
+
+//-- Obtener una colección con todos los elementos
+//-- de la clase digito
+digitos = document.getElementsByClassName("digito")
+
+//-- Establecer la misma función de retrollamada
+//-- para todos los botones de tipo dígito
+for (let button of digitos) {
+
+    //-- Se ejecuta cuando se pulsa un boton
+    //-- que es un dígito. Para que el código sea 
+    //-- mas legible la función de retrollamada se
+    //-- escribe como una función normal (digito)
+    button.onclick = digito;
+}
+
+//-------- Resto de funciones de retrollamada
+
+//-- Operación de sumar
+plus.onclick = (ev) => {
+
+    //-- Insertar simbolo de sumar
+    display.innerHTML += ev.target.value;
+
+    //-- ¡Ojo! Aquí se inserta el + siempre!
+    //-- Para que la calculadora funcione bien
+    //-- sólo se debe permitir insertar el operador
+    //-- en el estado OP1, y debe cambiar el estado
+    //-- a OPERATION (según el diagrama de estados)
+  
+}
+
+//-- Insertar simbolo de restar
+minus.onclick = (ev) => {
+    display.innerHTML += ev.target.value;
+  }
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+  
+    //-- Calcular la expresión y añadirla al display
+    display.innerHTML = eval(display.innerHTML);
+
+    //-- ¡Ojo! Aquí se hace siempre!
+    //-- Sólo se debe permitar que eso se haga
+    //-- si se está en el estado final (OP2)
+  
+}
+
+
+
+
+
+//-- Poner a cero la expresion
+//-- Y volver al estado inicial
+clear.onclick = () => {
+  display.innerHTML = "0";
+  estado = ESTADO.INIT;
+}
+
+
+  //-- Multiplicacion
+  multiply.onclick = () =>{
+      display.innerHTML += multiply.value;
+  }
+  
+  //-- Division
+  divide.onclick = () =>{
+      display.innerHTML += divide.value;
+  } 
+  
+  //-- Numero PI
+  pi.onclick = () =>{
+      display.innerHTML += pi.value;
+  } 
+  
+  //-- Borra ultimo caracter introducido
+  DELETE.onclick = () => {
+      display.innerHTML = display.innerHTML.slice(0,-1);
+  }
+  
+  //-- Punto
+  dot.onclick = () =>{
+      display.innerHTML += dot.value;
+  }
+  
+  
+  //-- Raiz cuadrada en la expresion
+  sqrt.onclick = () => {
+    display.innerHTML = Math.sqrt(display.innerHTML);
+  }
+  
