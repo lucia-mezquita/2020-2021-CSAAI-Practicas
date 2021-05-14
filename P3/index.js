6d2cabe18cefa24374777330119f1c471f11d805
lucia-mezquita <luciamezquita@hotmail.es>
2021-05-14 12:56:07 +0200

    js de nuestro index

---
+++ P3/index.js
@@ -0,0 +1,53 @@
+const canvas = document.getElementById("canvas");
+
+canvas.width = 600;
+canvas.height = 300;
+
+const ctx = canvas.getContext("2d");
+
+//-- Constantes de los ladrillos
+const LADRILLO = {
+    F: 7,  // Filas
+    C: 10,  // Columnas
+    w: 50,
+    h: 15,
+    origen_x: 0,
+    origen_y: 0,
+    padding: 8,
+    visible: true
+};
+
+//-- Estructura de los ladrillos
+const ladrillos = [];
+
+for (let i = 0; i < LADRILLO.F; i++) {
+    ladrillos[i] = [];
+    for (let j = 0; j < LADRILLO.C; j++) {
+      ladrillos[i][j] = {
+          x: (LADRILLO.w + LADRILLO.padding) * j,
+          y: (LADRILLO.h + LADRILLO.padding) * i,
+          w: LADRILLO.w,
+          h: LADRILLO.h,
+          padding: LADRILLO.padding,
+          visible: LADRILLO.visible
+        };
+    }
+}
+
+ladrillos[0][0].visible = false;
+
+
+//-- Dibujar ladrillos
+for (let i = 0; i < LADRILLO.F; i++) {
+    for (let j = 0; j < LADRILLO.C; j++) {
+
+      //-- Si el ladrillo es visible se pinta
+      if (ladrillos[i][j].visible) {
+        ctx.beginPath();
+        ctx.rect(ladrillos[i][j].x, ladrillos[i][j].y, LADRILLO.w, LADRILLO.h);
+        ctx.fillStyle = 'blue';
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+}
