9cd4e67ff463aed27705f2cb1b3e606227b92f4a
lucia-mezquita <luciamezquita@hotmail.es>
2021-05-20 14:28:45 +0200

    actualizando js para algunos botones

--- P4/index.js
+++ P4/index.js
@@ -5,11 +5,33 @@ const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
 
-//-- Acceso al deslizador
-const deslizador = document.getElementById('deslizador');
 
-//-- Valor del deslizador
-const range_value = document.getElementById('range_value');
+
+const imagen1 = document.getElementById('image1');
+const imagen2 = document.getElementById('image2');
+const imagen3 = document.getElementById('image3');
+
+const original = document.getElementById('original');
+const mirror = document.getElementById('mirror');
+const negative = document.getElementById('negative');
+const gray = document.getElementById('gray');
+
+
+
+//--Acceso deslizador
+const R = document.getElementById('R');
+const G = document.getElementById('G');
+const B = document.getElementById('B');
+const gray = document.getElementById('gray');
+
+
+//--Valores del deslizador
+const Range_Value_R = document.getElementById('Range_Value_R');
+const Range_Value_G = document.getElementById('Range_Value_G');
+const Range_Value_B = document.getElementById('Range_Value_B');
+const range_value_gray = document.getElementById('Range_Value_G');
+ 
+
 
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
@@ -31,7 +53,39 @@ img.onload = function () {
 
 
 //-- Funcion de retrollamada del deslizador
+
+//Funcion de retrollamada de los deslizadores
+R.oninput = () => {
+  colors();
+}
+G.oninput = () => {
+  colors();
+}
+B.oninput = () => {
+  colors();
+}
+
+
+//--Duncion habilitada para los deslizadores
+function dnables (){
+  R.disabled = false;
+  G.disabled = false;
+  B.disabled = false;
+}
+//--Funcion deshabilitada para los deslizadores
+function disabled(){
+
+  R.disabled = true;
+  G.disabled = true;
+  B.disabled = true;
+}
+
+
 deslizador.oninput = () => {
+
+
+
+
   //-- Mostrar el nuevo valor del deslizador
   range_value.innerHTML = deslizador.value;
 
@@ -56,6 +110,20 @@ deslizador.oninput = () => {
 
   //-- Poner la imagen modificada en el canvas
   ctx.putImageData(imgData, 0, 0);
+}
+
+
+
+
+//--Funcion creada para las diferentes elecciones de imagenes 
+image1.onclick = () => {
+  img.src="marrueco1.JPG";
+}
+image2.onclick = () => {
+  img.src="marruecos2.JPG";
+}
+image3.onclick = () => {
+  img.src="paris.JPG";
 }
 
 console.log("Fin...");
