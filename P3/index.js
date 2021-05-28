9da438c7f45eb567a72dff55b26621d79b88f3e9
lucia-mezquita <luciamezquita@hotmail.es>
2021-05-28 18:07:30 +0200

    sonido/tamaño canvas

--- P3/index.js
+++ P3/index.js
@@ -1,55 +1,18 @@
+console.log("Ejecutando JS...");
+
 const canvas = document.getElementById("canvas");
 
+//-- Sonidos
+const pong_raqueta = new Audio("pong-raqueta.mp3");
+const pong_rebote = new Audio("pong-rebote.mp3");
+const pong_tanto = new Audio("pong-tanto.mp3");
 
-//definimos el tamaño de nuestro canvas
-canvas.width = 600;
-canvas.height = 300;
 
+//-- Definir el tamaño del canvas
+canvas.width = 500;
+canvas.height = 600;
+
+
+//-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
-//-- Constantes de los ladrillos
-const LADRILLO = {
-    F: 7,  // Filas
-    C: 10,  // Columnas
-    w: 50,
-    h: 15,
-    origen_x: 0,
-    origen_y: 0,
-    padding: 8,
-    visible: true
-};
-
-//-- Estructura de los ladrillos
-const ladrillos = [];
-
-for (let i = 0; i < LADRILLO.F; i++) {
-    ladrillos[i] = [];
-    for (let j = 0; j < LADRILLO.C; j++) {
-      ladrillos[i][j] = {
-          x: (LADRILLO.w + LADRILLO.padding) * j,
-          y: (LADRILLO.h + LADRILLO.padding) * i,
-          w: LADRILLO.w,
-          h: LADRILLO.h,
-          padding: LADRILLO.padding,
-          visible: LADRILLO.visible
-        };
-    }
-}
-
-ladrillos[0][0].visible = false;
-
-
-//-- Dibujar ladrillos
-for (let i = 0; i < LADRILLO.F; i++) {
-    for (let j = 0; j < LADRILLO.C; j++) {
-
-      //-- Si el ladrillo es visible se pinta
-      if (ladrillos[i][j].visible) {
-        ctx.beginPath();
-        ctx.rect(ladrillos[i][j].x, ladrillos[i][j].y, LADRILLO.w, LADRILLO.h);
-        ctx.fillStyle = 'blue';
-        ctx.fill();
-        ctx.closePath();
-      }
-    }
-}
